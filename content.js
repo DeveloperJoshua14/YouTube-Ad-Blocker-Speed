32437b581e27d8c108b62cd11eb81d3570f17588
Joshua Nafziger <joshua@nafzigers.us>
2025-01-08 17:24:50 -0500

    Silly Error

--- content.js
+++ content.js
@@ -4,24 +4,25 @@ var VideoSpeed = -2;
 // Listen for changes in the YouTube video player
 const observer = new MutationObserver(() => {
   const video = document.querySelector("video");
-  console.log("-- Location 1: ");
-  console.log("-- video.playbackRate: ", video.playbackRate);
-  console.log("-- oldVideoSpeed: ", oldVideoSpeed);
-  console.log("-- VideoSpeed: ", VideoSpeed);
-  oldVideoSpeed = VideoSpeed;
-  VideoSpeed = video.playbackRate;
-  console.log("-- Location 2: ");
-  console.log("-- oldVideoSpeed: ", oldVideoSpeed);
-  console.log("-- VideoSpeed: ", VideoSpeed);
-  if (oldVideoSpeed == -2){
-    oldVideoSpeed = VideoSpeed;
-  }
-  console.log("-- Location 3: ");
-  console.log("-- video.playbackRate: ", video.playbackRate);
-  console.log("-- oldVideoSpeed: ", oldVideoSpeed);
-  console.log("-- VideoSpeed: ", VideoSpeed);
-
   if (video) {
+    console.log("-- Location 1: ");
+    console.log("-- video.playbackRate: ", video.playbackRate);
+    console.log("-- oldVideoSpeed: ", oldVideoSpeed);
+    console.log("-- VideoSpeed: ", VideoSpeed);
+    oldVideoSpeed = VideoSpeed;
+    VideoSpeed = video.playbackRate;
+    console.log("-- Location 2: ");
+    console.log("-- oldVideoSpeed: ", oldVideoSpeed);
+    console.log("-- VideoSpeed: ", VideoSpeed);
+    if (oldVideoSpeed == -2){
+      oldVideoSpeed = VideoSpeed;
+    }
+    console.log("-- Location 3: ");
+    console.log("-- video.playbackRate: ", video.playbackRate);
+    console.log("-- oldVideoSpeed: ", oldVideoSpeed);
+    console.log("-- VideoSpeed: ", VideoSpeed);
+
+  
     const isAd = document.querySelector('.ad-showing'); // Check if an ad is playing
     if (isAd) {
       console.log("-- ADD WAS LOADED");
